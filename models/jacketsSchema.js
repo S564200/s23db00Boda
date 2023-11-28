5fc2cac4dbd2757c23838ce4d2ae2c1adc700baf
S564200 <S564200@nwmissouri.edu>
2023-11-28 13:55:39 -0600

    added remaining parts of assignment13

--- models/jacketsSchema.js
+++ models/jacketsSchema.js
@@ -1,9 +1,20 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const jacketsSchema = mongoose.Schema({
-JacketName: String,
-JacketSize: String,
-JacketPrice: Number
-})
-module.exports = mongoose.model("jackets",jacketsSchema)
+  JacketName: {
+    type: String,
+    required: true
+  },
+  JacketSize: {
+    type: String,
+    required: true
+  },
+  JacketPrice: {
+    type: Number,
+    required: true,
+    min: [5.0, "The minimum value for JacketPrice is 5.0"],
+    max: [50.0, "The maximum value for JacketPrice is 50.0"]
+  }
+});
 
-
+module.exports = mongoose.model("Jacket", jacketsSchema);
--- routes/jackets.js
+++ routes/jackets.js
@@ -1,18 +1,24 @@
 var express = require('express');
 const jackets_controllers= require('../controllers/jackets');
 var router = express.Router();
-
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 // GET home page. 
 router.get('/', jackets_controllers.jackets_view_all_Page );
 
 /* GET detail costume page */
-router.get('/detail', jackets_controllers.jackets_view_one_Page);
+router.get('/detail',secured, jackets_controllers.jackets_view_one_Page);
 
-router.get('/create', jackets_controllers.jackets_create_Page);
+router.get('/create',secured, jackets_controllers.jackets_create_Page);
 
-router.get('/update', jackets_controllers.jackets_update_Page);
+router.get('/update',secured, jackets_controllers.jackets_update_Page);
 
-router.get('/delete', jackets_controllers.jackets_delete_Page);
+router.get('/delete',secured, jackets_controllers.jackets_delete_Page);
 
 
 
